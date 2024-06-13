3206f46512e90f0a5ab3bce0ac8f7cdbac18bd2c
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-06-13 14:49:56 -0300

    (temp) testado ID

--- src/main/java/com/lucas/model/Course.java
+++ src/main/java/com/lucas/model/Course.java
@@ -1,5 +1,6 @@
 package com.lucas.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -10,6 +11,7 @@ public class Course {
 
     @Id //indica a chave primária
     @GeneratedValue(strategy = GenerationType.AUTO) //faz uma sequência quando a gente faz um insert no banco de dados
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
